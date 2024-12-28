343e48a227676436d2d36db9e81c253ba43b47bb
Bhoomi Agrawal <bhoomiagrawal1212@gmail.com>
2024-12-28 18:04:39 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,13 @@
 python -m nltk.downloader punkt
 python -m nltk.downloader punkt_tab
 python -m nltk.downloader stopwords
+
+mkdir -p ~/.streamlit/
+
+echo "\
+[server]\n\
+port = $PORT\n\
+enableCORS = false\n\
+headless = true\n\
+\n\
+" > ~/.streamlit/config.toml
